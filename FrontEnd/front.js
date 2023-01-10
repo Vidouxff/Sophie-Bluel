f585169be886c424f7279dcfc00df9d38340ff51
Lucas Miranda <mirandalucas52@gmail.com>
2023-01-10 01:51:15 +0100

    version + dynamique de récupération des données

--- FrontEnd/front.js
+++ FrontEnd/front.js
@@ -1,41 +1,41 @@
-//récupère la gallerie
-const gallery = document.querySelector(".gallery");
+//fonction qui crée un élément "figure" dans la gallerie
+/*const createElement = (element) => {
+  const gallery = document.querySelector(".gallery");
+  gallery.innerHTML += `
+      <figure class="imageFilter filtre${element.categoryId}">
+          <img crossorigin="anonymous" src="${element.imageUrl}" alt="${element.title}">
+          <figcaption>${element.title}</figcaption>
+      </figure>`;
+};*/
 
-//crée un élément "figure" dans la gallerie
-function createElement(element){
-  gallery.innerHTML +=`<figure class="imageFilter filtre${element.categoryId}"> <img crossorigin="anonymous" src="${element.imageUrl}" alt="${element.title}"> <figcaption>${element.title}</figcaption> <figure>`
-}
+const createElement = (element) => {
+  const gallery = document.querySelector(".gallery");
+  gallery.innerHTML += `
+      <figure data-tag="${element.category.name}">
+          <img crossorigin="anonymous" src="${element.imageUrl}" alt="${element.title}">
+          <figcaption>${element.title}</figcaption>
+      </figure>`;
+};
 
-//récupère les boutons
-const buttons = document.querySelectorAll("button");
-
-//permet d'ajouter la classe active au premier bouton par défaut
-buttons[0].classList.add("active");
-
-//boucle pour parcourir tout les boutons et ajoute la classe active au bouton cliqué tout en retirant cette classe aux autres boutons
-buttons.forEach(button => {
-  button.addEventListener("click", function() {
-    buttons.forEach(element => element.classList.remove("active"));
-    this.classList.add("active");
-  });
-});
+//forEach
+//addEventListener
+//Data-
+//this this.dataset.tag
 
 //fonction qui va filtrer les photos par leur id
-function filtre(filtreId){
-
+/*function filtre(filtreId) {
   //récupère les photos
   const imageFilter = document.getElementsByClassName("imageFilter");
 
   //boucle pour vérifier les photos
-  for(i = 0; i < imageFilter.length; i++){
-
+  for (let i = 0; i < imageFilter.length; i++) {
     //si l'id correspond, la classe "filtered" est enlevé
-    if (imageFilter[i].classList.contains(filtreId)){
+    if (imageFilter[i].classList.contains(filtreId)) {
       imageFilter[i].classList.remove("filtered");
     }
 
     //si le bouton "tous" est selectionné, affiche toutes les images
-    else if (filtreId == "filtre0"){
+    else if (filtreId == "filtre0") {
       imageFilter[i].classList.remove("filtered");
     }
 
@@ -44,25 +44,97 @@ function filtre(filtreId){
       imageFilter[i].classList.add("filtered");
     }
   }
-}
+}*/
 
 fetch("http://localhost:5678/api/works")
-
   //si fetch fonctionne on récupère les données au format JSON
-  .then(function(res) {
+  .then((res) => {
     if (res.ok) {
       return res.json();
     }
   })
 
   //récupération de chaque élément et effectue la fonction "createElement" pour chaque élément récupéré
-  .then(function(element) {
-    for(i = 0; i < element.length; i++){
-      createElement(element[i])
-    }
+  .then((products) => {
+    products.forEach((product) => {
+      createElement(product);
+    });
   })
 
   //s'il y a une erreur, va console logger err
-  .catch(function(err) {
+  .catch(function (err) {
     console.log(err);
   });
+
+const createButton = (button) => {
+  const filtres = document.querySelector("#filtres");
+  filtres.insertAdjacentHTML(
+    "beforeend",
+    `<button data-tag="${button.name}"> ${button.name} </button>`
+  );
+
+  const buttons = document.querySelectorAll("nav button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      buttons.forEach((element) => element.classList.remove("active"));
+      this.classList.add("active");
+
+      const buttonTag = event.target.getAttribute("data-tag");
+      const images = document.querySelectorAll(".gallery figure");
+      images.forEach((image) => {
+        if (image.getAttribute("data-tag") === buttonTag) {
+          image.classList.remove("filtered");
+        } else {
+          image.classList.add("filtered");
+        }
+      });
+    });
+  });
+
+  function afficherToutesLesImages() {
+    // récupérer toutes les images
+    const toutesLesImages = document.querySelectorAll(".gallery figure");
+
+    // pour chaque image, lui ajouter la classe "visible" qui permet de l'afficher
+    toutesLesImages.forEach((image) => {
+      image.classList.remove("filtered");
+    });
+  }
+
+  // ajouter un écouteur d'événement "click" sur le bouton "Tous"
+  // qui appelle la fonction "afficherToutesLesImages" lorsqu'on clique sur le bouton
+  boutonTous.addEventListener("click", afficherToutesLesImages);
+};
+
+//le truc au dessus c'est faux, il faut je pense faire : addeventlistener pour chaque bouton, on enlève la classe filtered aux images qui ont la meme data-tag (.getAttribute) que le bouton cliqué, else on active filtered
+
+fetch("http://localhost:5678/api/categories")
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then((data) => {
+    data.forEach((button) => {
+      createButton(button);
+    });
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+//permet d'ajouter la classe active au bouton "Tous" par défaut
+const boutonTous = document.querySelector('button[data-tag="Tous"]');
+boutonTous.classList.add("active");
+//récupère les boutons
+
+//boucle pour parcourir tout les boutons et ajoute la classe active au bouton cliqué tout en retirant cette classe aux autres boutons
+
+//faire comme au dessus avec forEach, addEventListener et this.classlist pour filtrer par exemple pour chaque bouton, on écoute et si il y a click, on enleve avec this. la classe filtered. Du coup faut mettre dès le depart filtered aux boutons par défaut ou un truc comme ça
+
+//le truc au dessus c'est faux, il faut je pense faire : addeventlistener pour chaque bouton, on enlève la classe filtered aux images qui ont la meme data-tag (.getAttribute) que le bouton cliqué, else on active filtered
+
+//forEach
+//addEventListener
+//Data-
+//this this.dataset.tag
