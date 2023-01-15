65689d00e8762a4ab2d1627b3e55fb3e1531b9e4
Lucas Miranda <mirandalucas52@gmail.com>
2023-01-15 18:58:49 +0100

    ajout fonction logout et ajout dynamique de la barre si connecté

--- FrontEnd/front.js
+++ FrontEnd/front.js
@@ -24,7 +24,7 @@ fetch("http://localhost:5678/api/works")
   })
 
   //s'il y a une erreur, va console logger err
-  .catch(function (err) {
+  .catch((err) => {
     console.log(err);
   });
 
@@ -37,45 +37,6 @@ const createButton = (button) => {
     "beforeend",
     `<button data-tag="${button.name}"> ${button.name} </button>`
   );
-
-  //on récupère les boutons
-  const buttons = document.querySelectorAll("#filtres button");
-  //pour chaque bouton, si on clique dessus, fais une fonction
-  buttons.forEach((button) => {
-    button.addEventListener("click", function () {
-      //on enlève le classe "active" à tout les boutons
-      buttons.forEach((element) => element.classList.remove("active"));
-      //on met la classe "active" au bouton cliqué
-      this.classList.add("active");
-
-      //on récupère le tag du bouton
-      const buttonTag = this.dataset.tag;
-      //on récupère les figure faites plus haut
-      const images = document.querySelectorAll(".gallery figure");
-      //pour chaque figure on fait une fonction
-      images.forEach((image) => {
-        //si l'image a le même tag que le bouton, on enlève la classe "filtered"
-        if (image.getAttribute("data-tag") === buttonTag) {
-          image.classList.remove("filtered");
-          //on applique la classe "filtered" aux autres qui n'ont pas le même tag
-        } else {
-          image.classList.add("filtered");
-        }
-      });
-    });
-  });
-
-  //fonction qui va afficher toutes les images
-  function afficherToutesLesImages() {
-    // récupère toutes les images
-    const toutesLesImages = document.querySelectorAll(".gallery figure");
-    //on enlève la classe "filtered" a chaque images
-    toutesLesImages.forEach((image) => {
-      image.classList.remove("filtered");
-    });
-  }
-  //quand on clique sur "Tous", va faire la fonction afficherToutesLesImages
-  boutonTous.addEventListener("click", afficherToutesLesImages);
 };
 
 fetch("http://localhost:5678/api/categories")
@@ -93,6 +54,47 @@ fetch("http://localhost:5678/api/categories")
     });
   })
 
+  .then(() => {
+    //on récupère les boutons
+    const buttons = document.querySelectorAll("#filtres button");
+    //pour chaque bouton, si on clique dessus, fais une fonction
+    buttons.forEach((button) => {
+      button.addEventListener("click", function () {
+        //on enlève le classe "active" à tout les boutons
+        buttons.forEach((element) => element.classList.remove("active"));
+        //on met la classe "active" au bouton cliqué
+        this.classList.add("active");
+
+        //on récupère le tag du bouton
+        const buttonTag = this.dataset.tag;
+        //on récupère les figure faites plus haut
+        const images = document.querySelectorAll(".gallery figure");
+        //pour chaque figure on fait une fonction
+        images.forEach((image) => {
+          //si l'image a le même tag que le bouton, on enlève la classe "filtered"
+          if (image.getAttribute("data-tag") === buttonTag) {
+            image.classList.remove("filtered");
+            //on applique la classe "filtered" aux autres qui n'ont pas le même tag
+          } else {
+            image.classList.add("filtered");
+          }
+        });
+      });
+    });
+
+    //fonction qui va afficher toutes les images
+    function afficherToutesLesImages() {
+      // récupère toutes les images
+      const toutesLesImages = document.querySelectorAll(".gallery figure");
+      //on enlève la classe "filtered" a chaque images
+      toutesLesImages.forEach((image) => {
+        image.classList.remove("filtered");
+      });
+    }
+    //quand on clique sur "Tous", va faire la fonction afficherToutesLesImages
+    boutonTous.addEventListener("click", afficherToutesLesImages);
+  })
+
   //s'il y a une erreur, va console logger err
   .catch(function (err) {
     console.log(err);
@@ -101,3 +103,66 @@ fetch("http://localhost:5678/api/categories")
 //permet d'ajouter la classe active au bouton "Tous" par défaut
 const boutonTous = document.querySelector('button[data-tag="Tous"]');
 boutonTous.classList.add("active");
+
+const modifierContent = () => {
+  const body = document.querySelector("#body");
+  const article = document.querySelector("article");
+  const imgPresentation = document.querySelector("#imgPresentation");
+  const projetsTitle = document.querySelector("#mesProjets");
+  body.insertAdjacentHTML(
+    "beforebegin",
+    `<div id="editionBarre">
+        <span class="edition">
+          <i class="fa-regular fa-pen-to-square"></i> Mode édition
+        </span>
+        <button id="buttonChangement">publier les changements</button>
+      </div>`
+  );
+  article.insertAdjacentHTML(
+    "afterbegin",
+    `<a href="#" class="modifier">
+      <i class="fa-regular fa-pen-to-square"></i> modifier
+    </a>`
+  );
+  imgPresentation.insertAdjacentHTML(
+    "afterend",
+    `<a href="#" class="modifier modifierFigure">
+      <i class="fa-regular fa-pen-to-square"></i> modifier
+    </a>`
+  );
+  projetsTitle.insertAdjacentHTML(
+    "afterend",
+    `<a href="#" class="modifier modifierFigure">
+      <i class="fa-regular fa-pen-to-square"></i> modifier
+    </a>`
+  );
+  document.getElementById(
+    "loginButton"
+  ).innerHTML = `<a id="logoutButton">logout</a>`;
+  document.getElementById("filtres").style.display = "none";
+};
+
+const loggedIn = () => {
+  const token = sessionStorage.getItem("token");
+  if (!token) {
+    return 0;
+  } else if (token) {
+    modifierContent();
+  }
+};
+
+loggedIn();
+
+const logout = () => {
+  // Récupération du bouton "logout"
+  const logoutButton = document.getElementById("logoutButton");
+  // Ajout d'un écouteur d'événement au clic sur le bouton
+  logoutButton.addEventListener("click", () => {
+    // Suppression du token de sessionStorage
+    sessionStorage.removeItem("token");
+    // Redirection vers la page de connexion
+    window.location.href = "login.html";
+  });
+};
+
+logout();
--- FrontEnd/login/login.js
+++ FrontEnd/login/login.js
@@ -1,14 +1,42 @@
-const login = () => {
-  // Récupération des informations d'identification
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
+const email = document.getElementById("email"); //récupère le champ de l'email
+const password = document.getElementById("password"); //récupère le champ du mot de passe
+const form = document.querySelector("form"); //récupère le formulaire
 
-  // Vérification des informations d'identification
-  if (email === "sophie.bluel@test.tld" && password === "S0phie") {
-    // Redirection vers la page d'admin
-    window.location.href = "http://127.0.0.1:5500/FrontEnd/index.html";
-  } else {
-    // Affichage d'une alerte "user not found"
-    alert("user not found");
-  }
-};
+//ajoute un addEventListener au formulaire. Lorsque le formulaire est soumis, la fonction d'après est executée
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); //empêche la soumission par défaut du formulaire, ce qui permet de contrôler ce qui se passe lorsque le formulaire est soumis.
+
+  //crée un objet user avec les valeurs d'email et de mot de passe entrées dans le formulaire.
+  const user = {
+    email: email.value,
+    password: password.value,
+  };
+
+  fetch("http://localhost:5678/api/users/login", {
+    //la méthode est de type POST
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(user), //ajoute user en format JSON
+  })
+    .then((response) => {
+      //si la réponse à réussi on on récupère le contenu au format JSON
+      if (response.status == 200) {
+        return response.json();
+        //sinon, affiche un message d'erreur
+      } else {
+        alert("Erreur dans l'identifiant ou le mot de passe");
+      }
+    })
+
+    .then((data) => {
+      sessionStorage.setItem("token", data.token); //stock le token dans un session storage.
+      document.location.href = "/FrontEnd/index.html"; //redirige vers la page index.html
+    })
+
+    //s'il y a une erreur, va console logger err
+    .catch((err) => {
+      console.log(err);
+    });
+});
